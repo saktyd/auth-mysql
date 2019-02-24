dff4828c27885d9cdd8b91c706a2b65fb415b880
Sakti Dewantoro <saktyd@gmail.com>
2019-02-24 19:11:57 +0700

    implemnt getHello, getAllUser, getUserById, deleteUserById createNewUser

---
+++ index.js
@@ -0,0 +1,20 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+app.use(bodyParser.json())
+const port = 8000
+
+const all = require('./middlewares')
+const users = require('./middlewares/users/index')
+
+app.get('/', all.getHello)
+app.get('/users', users.getAllUser)
+app.get('/users/:id', users.getUserById)
+app.delete('/users/:id', users.deleteUserById)
+app.post('/users/register', users.createNewUser)
+
+app.listen(port, () => {
+  console.log(`Express app is listening on localhost:${port}`)
+})
---
+++ middlewares/users/index.js
@@ -0,0 +1,50 @@
+const knex = require('knex')({
+  client: 'mysql2',
+  connection: {
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'yourusername',
+    password: process.env.DB_PASSWORD || 'yourpassword',
+    database: process.env.DB_DATABASE || 'yourdatabasename'
+  }
+})
+
+module.exports = {
+  //Get list of users
+  getAllUser: async (req, res) => {
+    res.send({
+      message: 'List of all users',
+      users: await knex('users').select()
+    })
+  },
+
+  createNewUser: async (req, res) => {
+    res.send({
+      message: 'Register',
+      user: await knex('users').insert({
+        name: req.body.name,
+        email: req.body.email
+      })
+    })
+  },
+
+  //Get user by id
+  getUserById: async (req, res) => {
+    res.send({
+      message: 'List of all users',
+      user: await knex
+        .select()
+        .from('users')
+        .where('id', Number(req.params.id))
+    })
+  },
+
+  //Delete user by id
+  deleteUserById: async (req, res) => {
+    res.send({
+      message: 'One user has been deleted',
+      user: await knex('users')
+        .where('id', req.params.id)
+        .del()
+    })
+  }
+}
